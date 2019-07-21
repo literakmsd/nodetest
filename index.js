e3d744ce827fe2b1ee58a0d6320399531acd9875
Litera Institute <literakmsd@gmail.com>
2019-07-21 20:43:37 +0700

    enable testing

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ app.listen(3000,()=>console.log("listening on 3000"))
 app.get('/',(req,res)=>{
     res.send('Hallo world')
 })
+
+app.get('/loaderio-93487e8f4d08ff7c50435fede0bb5b5d.html',(req,res)=>{
+    res.send("loaderio-93487e8f4d08ff7c50435fede0bb5b5d")
+})
